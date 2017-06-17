674b1300e68b7fe7309f9fb8a673fb83ebad2740
Francois Catuhe <fcatuhe@gmail.com>
2017-06-17 14:09:12 +0200

    install pundit

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,15 @@
 class ApplicationController < ActionController::Base
+  include Pundit
+
   protect_from_forgery with: :exception
-  before_action :authenticate_user!
+
   before_action :set_locale
+  before_action :authenticate_user!
+
+  after_action :verify_authorized, except: :index, unless: :skip_pundit?
+  after_action :verify_policy_scoped, only: :index, unless: :skip_pundit?
+
+  rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
 
   def set_locale
     I18n.locale = params.fetch(:locale, I18n.default_locale).to_sym
@@ -9,5 +17,16 @@ class ApplicationController < ActionController::Base
 
   def default_url_options
     { locale: I18n.locale == I18n.default_locale ? nil : I18n.locale }
+  end
+
+  def user_not_authorized
+    flash[:alert] = t 'user_not_authorized'
+    redirect_to(root_path)
+  end
+
+  private
+
+  def skip_pundit?
+    devise_controller? || params[:controller] =~ /(^(rails_)?admin)|(^pages$)/
   end
 end
