8d453d53d7e2d7c35986cb380b3019815920ea6d
Francois Catuhe <fcatuhe@gmail.com>
2017-06-17 15:02:21 +0200

    install facebook connect

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -8,4 +8,30 @@ module ApplicationHelper
       default
     end
   end
+  def avatar_tag(user, options = {})
+    if user.photo
+      options = {width: 256, height: 256, crop: :fill}.merge(options)
+      cl_image_tag user.photo.path, options
+    elsif user.facebook_picture_url
+      image_tag user.facebook_picture_url, options
+    else
+      image_tag 'logo.png', options
+    end
+  end
+
+  def cl_image_path_with_default(photo, options = {})
+    if photo
+      cl_image_path photo.path, options
+    else
+      image_path 'logo.png', options
+    end
+  end
+
+  def cl_image_tag_with_default(photo, options = {})
+    if photo
+      cl_image_tag photo.path, options
+    else
+      image_tag 'logo.png', options
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
 
   scope '(:locale)', locale: /en/ do
     root to: 'pages#home'
