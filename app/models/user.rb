7fbc19d3128dd0173e33169ed6f33968affc1f3b
Francois Catuhe <fcatuhe@gmail.com>
2017-06-17 14:22:28 +0200

    add role to users

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,8 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  validates :role, presence: true
+
+  enum role: { customer: 0, admin: 9 }
 end
