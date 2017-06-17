96cc0813907252f11180c56a2e454fcfff3514d9
Francois Catuhe <fcatuhe@gmail.com>
2017-06-17 21:17:45 +0200

    implement mailer and welcome email

--- Gemfile
+++ Gemfile
@@ -43,6 +43,7 @@ group :development, :test do
   gem 'better_errors'
   gem 'binding_of_caller'
   gem 'bullet'
+  gem 'letter_opener'
   gem 'listen', '~> 3.0.5'
   gem 'pry-byebug'
   gem 'pry-rails'
--- app/models/user.rb
+++ app/models/user.rb
@@ -14,6 +14,8 @@ class User < ApplicationRecord
   geocoded_by :address
   after_validation :geocode, if: :address_changed?
 
+  after_create :send_welcome_email
+
   def self.find_for_facebook_oauth(auth)
     user_params = auth.slice(:provider, :uid)
     user_params.merge! auth.info.slice(:email, :first_name, :last_name)
@@ -33,5 +35,11 @@ class User < ApplicationRecord
     end
 
     user
+  end
+
+  private
+
+  def send_welcome_email
+    UserMailer.welcome(self).deliver_later
   end
 end
